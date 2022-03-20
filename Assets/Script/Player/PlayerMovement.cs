fa0d0126b3f50d9e1dd25ec5fb5b273f6c70da69
KienanUnique <69349336+KienanUnique@users.noreply.github.com>
2022-03-20 22:50:46 +0500

    Add attack button

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 namespace Script.Player
 {
---
+++ Assets/Script/UI/UIButtons.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Script.UI
+{
+    public class UIButtons : MonoBehaviour
+    {
+        public void OnAttackButtonPress(){
+            Debug.Log("Attack button pressed");
+        }
+    }
+}
